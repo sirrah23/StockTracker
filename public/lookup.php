148bb3eee3118b6330f731cf7e2b508ed990b2dc
sirrah23 <hseth93@gmail.com>
2017-01-23 21:49:46 -0500

    Added buy stock form

--- public/lookup.php
+++ public/lookup.php
@@ -22,14 +22,20 @@
         return $data_assoc;
     };
     $stock_info = stock_data($stockname);
+    var_dump($stock_info);
 ?>
 
 <div id="stockinfo">
-    <?php
-        echo("The requested stock is ".$stock_info["stock_name"].".</br>");
-        echo("The asking price is $".$stock_info["asking_price"].".</br>");
-        echo("The bidding price is $".$stock_info["bidding_price"].".</br>");
-    ?>
+        <h1><?php echo($stock_info["stock_name"]); ?></h1>
+        <h2><?php echo("Asking Price: $".$stock_info["asking_price"]); ?></h2>
+        <h2><?php echo("Bidding Price: $".$stock_info["bidding_price"]); ?></h2>
+        <?php if($stock_info["asking_price"] != "N/A"):?>
+            <form action="buystock.php" method="post">
+                <input type="hidden" name="stockname" value=<?php echo("\"".$stock_info["stock_name"]."\""); ?> />
+                <input type="hidden" name="askingprice" value=<?php echo("\"".$stock_info["asking_price"]."\""); ?> />
+                <input type="submit" value="Buy" />
+            </form>
+        <?php endif; ?>
 </div>
 
 <?php require("../common/footer.php"); ?>
